dcd595f4ff3cf09e6794b02457544cf11f8e7183
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-23 09:05:54 +0100

    Add tests for adding doctors and patients in Admin class

--- codebase/test/testadmin.py
+++ codebase/test/testadmin.py
@@ -1,13 +1,19 @@
 import unittest
 
+from administration import Admin
+
 
 class MyTestAdmin(unittest.TestCase):
 
     def SetUp(self):
-        admin = Admin()
+        self.admin = Admin()
 
     def test_that_admin_can_add_a_doctor_to_the_doctor_database(self):
-        self.assertEqual(True, False)
+        self.admin.add_doctor("Dr. Smith", "Cardiology")
+
+
+    def test_that_admin_can_add_a_patient_to_the_patient_database(self):
+        self.admin.add_patient("John Doe", "123-456-7890")
 
 
 if __name__ == '__main__':
