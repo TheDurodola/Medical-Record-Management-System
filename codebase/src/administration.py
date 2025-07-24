17fa1f48411089dec46ba568e94b5ec334f670cc
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-24 14:25:19 +0100

    Add methods to Admin class for adding doctors and checking database size

--- codebase/src/administration.py
+++ codebase/src/administration.py
@@ -1,3 +1,9 @@
+import patient_database
+import doctor_database
+
 class Admin:
-    def add_doctor(self, first_name, last_name, specialization, phone_number=None, email=None):
-        pass
+    def add_doctor(self, id_number, first_name, last_name, specialization, phone_number):
+        doctor_database.add_doctor_record(id_number, first_name, last_name, specialization, phone_number)
+
+    def check_doctor_database_size(self):
+        return doctor_database.check_doctor_database_size()
