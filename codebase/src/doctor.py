8c1c0b6d9577ae8516dfa6250bd41a1057f2ce49
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-26 05:41:57 +0100

    Refactored `Doctor` class to include `doctor_id` and `phone_number`; added property methods for better attribute management.

--- codebase/src/doctor.py
+++ codebase/src/doctor.py
@@ -1,17 +1,38 @@
 class Doctor:
-    def __init__(self, first_name, last_name, specialization):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.specialization = specialization
+    def __init__(self, doctor_id, first_name, last_name, specialization, phone_number):
+        self.doctor_id = doctor_id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._specialization = specialization
+        self._phone_number = phone_number
 
-    def get_first_name(self):
-        return self.first_name
+    def get_doctor_id(self):
+        return self.doctor_id
 
-    def get_last_name(self):
-        return self.last_name
 
-    def get_specialization(self):
-        return self.specialization
+    @property
+    def first_name(self):
+        return self._first_name
 
-    def view_specialization(self):
-        print(self.specialization)
+    @first_name.setter
+    def first_name(self, first_name):
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self._last_name = last_name
+
+    @property
+    def specialization(self):
+        return self._specialization
+
+    @specialization.setter
+    def specialization(self, specialization):
+        self._specialization = specialization
+
+    def get_doctor_info(self):
+        return f"Doctor ID: {self.doctor_id}\nFull-name: Dr. {self.first_name} {self.last_name}\nSpecialization: {self.specialization}\nPhone No{self.phone_number}"
