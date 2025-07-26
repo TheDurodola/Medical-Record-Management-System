1da6ee314de94b5316a14eb1a06ab6d663ff06e1
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-26 07:06:42 +0100

    Added new test cases for doctor management in `test_administration.py` (register, clear, update, and multiple registration).

--- codebase/test/test_administration.py
+++ codebase/test/test_administration.py
@@ -24,9 +24,34 @@ class MyTestAdmin(unittest.TestCase):
         self.assertEqual(2, self.admin.check_patient_database_size())
         self.assertEqual({"PT1":2, "PT2":3}, self.admin.retrieve_all_patient_records())
 
-    def test_that_a_new_doctor_doctor_can_be_registered(self):
+    def test_that_a_new_doctor_can_be_registered(self):
         self.admin.register_doctor("Oginni", "Fiyinfoluwa", "Oncology", "08148260470")
         self.assertEqual(1, self.admin.check_doctor_database_size())
+
+    def test_that_doctor_database_can_be_cleared(self):
+        self.admin.clear_doctor_database()
+        self.assertEqual(0, self.admin.check_doctor_database_size())
+        self.admin.register_doctor("Oginni", "Fiyinfoluwa", "Oncology", "08148260470")
+        self.assertEqual(1, self.admin.check_doctor_database_size())
+        self.admin.clear_doctor_database()
+        self.assertEqual(0, self.admin.check_doctor_database_size())
+
+
+    def test_that_multiple_doctors_can_be_registered(self):
+        self.admin.clear_doctor_database()
+        self.assertEqual(0, self.admin.check_doctor_database_size())
+        self.admin.register_doctor("Oginni", "Fiyinfoluwa", "Oncology", "08148260470")
+        self.admin.register_doctor("Doe", "John", "Cardiology", "08123456789")
+        self.assertEqual(2, self.admin.check_doctor_database_size())
+
+
+    def test_that_doctor_record_can_be_update(self):
+        self.admin.clear_doctor_database()
+        self.admin.register_doctor("Aginni", "Fiyinfoluwa", "Oncology", "08148260470")
+        self.admin.update_doctor_record("DR1", "Oginni", "Fiyinfoluwa", "Oncology", "08148260470")
+        self.assertEqual("Oginni")
+
+
 
 
 
