1a8c4e0dc87eee7c3c2908de0b17e44fdcf2741b
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-07-26 07:49:49 +0100

    Refactored `Doctor` class to use private attributes with getter and setter methods for better encapsulation.

--- codebase/src/doctor.py
+++ codebase/src/doctor.py
@@ -1,38 +1,37 @@
 class Doctor:
     def __init__(self, doctor_id, first_name, last_name, specialization, phone_number):
-        self.doctor_id = doctor_id
-        self._first_name = first_name
-        self._last_name = last_name
-        self._specialization = specialization
-        self._phone_number = phone_number
+        self.__doctor_id = doctor_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__specialization = specialization
+        self.__phone_number = phone_number
 
     def get_doctor_id(self):
-        return self.doctor_id
+        return self.__doctor_id
 
+    def get_first_name(self):
+        return self.__first_name
 
-    @property
-    def first_name(self):
-        return self._first_name
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
 
-    @first_name.setter
-    def first_name(self, first_name):
-        self._first_name = first_name
+    def get_last_name(self):
+        return self.__last_name
 
-    @property
-    def last_name(self):
-        return self._last_name
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
 
-    @last_name.setter
-    def last_name(self, last_name):
-        self._last_name = last_name
+    def get_specialization(self):
+        return self.__specialization
 
-    @property
-    def specialization(self):
-        return self._specialization
+    def set_specialization(self, specialization):
+        self.__specialization = specialization
 
-    @specialization.setter
-    def specialization(self, specialization):
-        self._specialization = specialization
+    def get_phone_number(self):
+        return self.__phone_number
+
+    def set_phone_number(self, phone_number):
+        self.__phone_number = phone_number
 
     def get_doctor_info(self):
-        return f"Doctor ID: {self.doctor_id}\nFull-name: Dr. {self.first_name} {self.last_name}\nSpecialization: {self.specialization}\nPhone No{self.phone_number}"
+        return f"Doctor ID: {self.__doctor_id}\nFull-name: Dr. {self.__first_name} {self.__last_name}\nSpecialization: {self.__specialization}\nPhone No{self.__phone_number}"
